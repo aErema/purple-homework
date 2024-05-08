c2fa5ef0c4bc4303f036d89f85d43fd64719eba3
nutkagorskaya <anutagor@yandex.ru>
2024-05-08 10:03:17 +0300

    5.1 homework

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 'use strict'
 
-const arrayOfObjects = [
-        {id: 1, name: "Вася"},
-        {id: 2, name: "Петя"},
-        {id: 1, name: "Вася"},
-];
-
-console.log(unionArrayOfObjects(arrayOfObjects));
-
-function unionArrayOfObjects(array) {
-    const checkSet = new Set(array.map((item) => item.id));
-    let arrayOfObjects = [];
-    checkSet.forEach((value) => {
-        arrayOfObjects = [...arrayOfObjects, array.find(item => item.id === value)];
-    });
-    return arrayOfObjects;
+function dice(type){
+    const typeObject = ['d4', 'd6', 'd8', 'd10', 'd12', 'd16', 'd20'];
+    if (!typeObject.find(t => t === type.toLowerCase())) {
+        return 'Тип не поддерживается';
+    }
+    const max = +type.slice(1);
+    return Math.floor(Math.random() * max) + 1;
 }
+
+console.log(dice('D8'));
+console.log(dice('D4'));
+console.log(dice('D11'));
+console.log(dice('D20'));
